be244a41cdfbfcf8c1a6bafaf4f9b84927593337
Alicia Rannou <Alicia Rannou>
2017-04-27 09:47:20 +0200

    windows bug fixed

--- Model/Service/implementationBorrowService_Dummy.php
+++ Model/Service/implementationBorrowService_Dummy.php
@@ -73,9 +73,8 @@ class implementationBorrowService_Dummy implements interfaceBorrowService
       }
 
 
-
     //on emprunte toujours un trousseau
-    public function borrowKeychain(int $userId,int $keychainId,DateTime $dueDate)
+    public function borrowKeychain($userId,$keychainId,DateTime $dueDate)
     {
       $tDate = new DateTime;
       $tDate->setTimestamp(time());
