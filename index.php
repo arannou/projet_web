b4acb4c6e784845d89958a6d12c1902da58556f7
Anaïs Pignet <Anas Pignet>
2017-05-12 14:22:31 +0200

    Anais : ajout commentaires

--- index.php
+++ index.php
@@ -6,28 +6,33 @@ require_once 'Libs/limonade/limonade.php';
 
 dispatch('/', 'home');
 function home(){
+    //Import des classes
     require_once 'Model/DAO/implementationKeychainDAO_Dummy.php';
     require_once 'Model/DAO/implementationUserDAO_Dummy.php';
     require_once 'Model/VO/KeychainVO.php';
     require_once 'Model/Service/implementationBorrowService_Dummy.php';
     require_once 'Controller/HomeController.php';
 
+    //Appel du controlleur
     $controller = new HomeController("Home");
 
+    //Appel de la vue
     require 'View/home.php';
 }
 
 dispatch('/users', 'users');
 function users(){
+    //Import des classes
     require_once 'Model/DAO/implementationUserDAO_Dummy.php';
     require_once 'Model/VO/UserVO.php';
     require_once 'Controller/UsersController.php';
-
+    //Appel du controlleur
     $controller = new UsersController("Users");
-
+    //Appel de la vue
     require 'View/users.php';
 }
 
+//Demarrage de Limonade
 run();
 
 ?>
