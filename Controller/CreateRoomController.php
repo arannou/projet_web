4f30895273c7d3da0861bf032b1d6c1cefd724f7
Anais Pignet <anais.pignet@hotmail.fr>
2017-05-28 22:52:48 +0200

    Commencement formulaire création salle - Vue

--- Controller/CreateRoomController.php
+++ Controller/CreateRoomController.php
@@ -4,8 +4,6 @@ class CreateRoomController {
     public function __construct(){
         $createRoom    = implementationRoomService::getInstance();
         $createRoom->createRoom($_POST['roomName']);
-        var_dump($_SESSION);
-
     }
 
 
