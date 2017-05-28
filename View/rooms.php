3bfc5a1509b4b6d8a6c3e14893a6372f7c964319
Anais Pignet <anais.pignet@hotmail.fr>
2017-05-28 22:52:48 +0200

    Commencement formulaire création salle - Vue

--- View/rooms.php
+++ View/rooms.php
@@ -10,20 +10,34 @@
           <div class="x_title">
             Liste des salles
           </div>
-<table class="table">
-    <thead>
-        <th>nom</th>
-    </thead>
-<?php
-    foreach ($controller->rooms as $room) {
-        echo '<tr>';
-            echo '<td>'.$room->getId().'</td>';
-        echo '</tr>';
-    }
-?>
-</table>
-</div>
-</div>
-</div>
-</div>
+          <table class="table">
+            <thead>
+              <th>nom</th>
+            </thead>
+            <?php
+            foreach ($controller->rooms as $room) {
+              echo '<tr>';
+              echo '<td>'.$room->getId().'</td>';
+              echo '</tr>';
+            }
+            ?>
+          </table>
+        </div>
+        <div class="x_panel">
+          <div class="x_title">
+            Ajout d'une salle
+          </div>
+          <div class="form-group">
+            <form action="?/rooms" method="post" data-parsley-validate class="form-horizontal form-label-left">
+              <label class="control-label col-md-3 col-sm-3 col-xs-12">Nom de la salle :</label>
+              <div class="col-md-6 col-sm-6 col-xs-12">
+                <input type="text" name="roomName" placeholder="Numéro complet, exemple : 001H">
+              </div>
+              <input type="submit" value="Ajouter" class="btn btn-warning" >
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
