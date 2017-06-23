07ae0e1f6a66308cbf1fa5566ca4b753d59add94
= <luuka.tr@gmail.com>
2017-06-23 12:14:29 +0200

    Fix references errors

--- Model/DAO/implementationKeyKeychainDAO_Session.php
+++ Model/DAO/implementationKeyKeychainDAO_Session.php
@@ -2,51 +2,42 @@
 
 require_once 'Model/DAO/interfaceKeyKeychainDAO.php';
 require_once 'Model/DAO/implementationKeyDAO_Session.php';
-require_once 'Model/DAO/ImplementationDAO_MYSQL.php';
 
-class implementationKeyKeychainDAO_MYSQL extends ImplementationDAO_MYSQL implements interfaceKeyKeychainDAO
+class implementationKeyKeychainDAO_Session implements interfaceKeyKeychainDAO
 {
 
     private static $_instance;
-    private $_tableName = "keys_keychain";
 
     private $_keyDAO;
 
     private function __construct(){
-        $factory = getDAOFactory();
-        $this->_keyDAO = $factory->getKeyDAO();
+        parent::initDb();
+        $this->_keyDAO = implementationKeyDAO_Session::getInstance();
     }
 
     public static function getInstance(){
         if(is_null(self::$_instance)) {
-            self::$_instance = new implementationKeyKeychainDAO_MYSQL();
+            self::$_instance = new implementationKeyKeychainDAO_Session();
         }
         return self::$_instance;
     }
 
     public function create($key, $keyChain){
-        $stmt = $this->pdo->prepare("INSERT INTO $this->_tableName
-			(keyId, keychainId)
-			VALUES (:keyId, :keychainId)");
+        $relation = [];
+        $relation['keyId'] = $key->getId();
+        $relation['keychainId'] = $keyChain->getId();
 
-        $stmt->bindParam(':keyId', $key->getId());
-        $stmt->bindParam(':keychainId', $keyChain->getId());
-
-        $stmt->execute();
+        array_push($_SESSION['keyKeychain'], $relation);
     }
 
     public function getKeysByKeychainId($keychainId){
         $keys = [];
-
-        $stmt = $this->pdo->prepare("SELECT * FROM $this->_tableName WHERE keychainId = :kid");
-        $stmt->bindParam(":kid", $keychainId);
-        $stmt->execute();
-
-        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            $k = $this->_keyDAO->getKeyById($row['keyId']);
-            array_push($keys, $k);
+        foreach ($_SESSION['keyKeychain'] as $index => $key) {
+            if($key['keychainId'] == $keychainId){
+                $k = $this->_keyDAO->getKeyById($key['keyId']);
+                array_push($keys, $k);
+            }
         }
-
         return $keys;
     }
 
